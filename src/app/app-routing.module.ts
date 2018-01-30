772c77db78526a98d32cd3f76bc71207e775334e
RockerHX <rockerhx@gmail.com>
2018-01-30 10:44:21 +0800

    Add[Route]: BasicLayoutComponent
    /basic-layout -> BasicLayoutComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { BasicLayoutComponent } from './basic-layout/basic-layout.component';
+
+const routes: Routes = [
+  {path: 'basic-layout', component: BasicLayoutComponent}
+];
+
 @NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [
     RouterModule
   ],
